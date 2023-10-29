8213d8f4ab6f79b32dc43ac7c66074d0ad16981e
dustin graham <thedustingraham@gmail.com>
2023-10-29 17:07:37 -0600

    remove registration

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,7 +1,7 @@
 import { CognitoIdentityServiceProvider, DynamoDB } from 'aws-sdk';
 import { AuthenticationResultType } from 'aws-sdk/clients/cognitoidentityserviceprovider';
 import { UserRepository } from '../repositories/UserRepository';
-import { AuthRequest, RegisterRequest } from '../models/auth';
+import { AuthRequest } from '../models/auth';
 
 export class AuthService {
   userRepo: UserRepository;
@@ -38,48 +38,5 @@ export class AuthService {
       throw new Error('An error occured');
     }
     return result;
-  }
-
-  async register(body: RegisterRequest): Promise<void> {
-    const params = {
-      UserPoolId: this.userPoolId,
-      Username: body.email,
-      UserAttributes: [
-        {
-          Name: 'email',
-          Value: body.email,
-        },
-        {
-          Name: 'preferred_username',
-          Value: body.username,
-        },
-        {
-          Name: 'email_verified',
-          Value: 'true',
-        },
-      ],
-      MessageAction: 'SUPPRESS',
-    };
-    const user = (await this.cognito.adminCreateUser(params).promise()).User;
-    if (!user) {
-      throw new Error('Error creating user');
-    }
-    const sub = user.Attributes?.find((attr) => attr.Name === 'sub')?.Value;
-    if (!sub) {
-      throw new Error('Error creating user');
-    }
-
-    await this.userRepo.saveUser({
-      userId: sub,
-      userName: body.email,
-      preferredUserName: body.username,
-    });
-    const Params = {
-      Password: body.password,
-      UserPoolId: this.userPoolId,
-      Username: body.email,
-      Permanent: true,
-    };
-    await this.cognito.adminSetUserPassword(Params).promise();
   }
 }
