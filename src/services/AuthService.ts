14a5ae5005b5a72b017b6327ee00cac0760c0bac
dustin graham <thedustingraham@gmail.com>
2023-10-29 17:09:14 -0600

    cleanup

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,26 +1,14 @@
-import { CognitoIdentityServiceProvider, DynamoDB } from 'aws-sdk';
+import { CognitoIdentityServiceProvider } from 'aws-sdk';
 import { AuthenticationResultType } from 'aws-sdk/clients/cognitoidentityserviceprovider';
-import { UserRepository } from '../repositories/UserRepository';
 import { AuthRequest } from '../models/auth';
 
 export class AuthService {
-  userRepo: UserRepository;
-
   cognito: CognitoIdentityServiceProvider;
-
-  dynamoDb: DynamoDB.DocumentClient;
-
-  userPoolId: string;
 
   clientId: string;
 
-  userTable = 'Users';
-
   constructor() {
-    this.userRepo = new UserRepository();
     this.cognito = new CognitoIdentityServiceProvider();
-    this.dynamoDb = new DynamoDB.DocumentClient();
-    this.userPoolId = process.env.COGNITO_USER_POOL_ID ? process.env.COGNITO_USER_POOL_ID : '';
     this.clientId = process.env.COGNITO_CLIENT_ID ? process.env.COGNITO_CLIENT_ID : '';
   }
 
